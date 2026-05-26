c0a89cbcc66bd060ecb795d87690caecadf7fea3
Mohammad-Albayaydah <zastzsha123133@gmail.com>
2026-05-26 13:55:15 +0300

    Resolve url helper conflict

--- src/helpers/url_helper.jsx
+++ src/helpers/url_helper.jsx
@@ -149,12 +149,11 @@ export const QUOTATION_PRICING_DECISION = quotationId =>
   `/quotation-pricing/quotation/${quotationId}/decision`;
 export const QUOTATION_PRICING_CANCEL = quotationId =>
   `/quotation-pricing/quotation/${quotationId}/cancel`;
-<<<<<<< HEAD
-export const QUOTATION_FINAL_PRICING = (quotationId) =>
+export const QUOTATION_FINAL_PRICING = quotationId =>
   `/quotation-pricing/quotation/${quotationId}/final`;
 
 // RESERVATION FILES
 export const RESERVATION_FILES = "/reservation-files";
-export const RESERVATION_FILE_BY_ID = (id) => `/reservation-files/${id}`;
-export const RESERVATION_FILE_FROM_QUOTATION = (quotationId) =>
+export const RESERVATION_FILE_BY_ID = id => `/reservation-files/${id}`;
+export const RESERVATION_FILE_FROM_QUOTATION = quotationId =>
   `/reservation-files/from-quotation/${quotationId}`;
